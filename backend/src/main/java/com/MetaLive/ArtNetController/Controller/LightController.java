dbc5fd0beba9f86ff0da814dc316cd2f09c83d4b
W1EMPTY <12318385+w1empty@user.noreply.gitee.com>
2025-06-11 16:08:05 +0800

    backend init

--- backend/src/main/java/com/MetaLive/ArtNetController/Controller/LightController.java
+++ backend/src/main/java/com/MetaLive/ArtNetController/Controller/LightController.java
@@ -17,37 +17,37 @@ public class LightController {
     @Autowired
     private LightService lightService;
     private ArtNetClient artNetClient;
+
     @GetMapping("/lights")
     public ResponseEntity<List<Light>> getAllLights() {
-        List<Light> lights= lightService.findAllLights();
+        List<Light> lights = lightService.findAllLights();
         return ResponseEntity.ok(lights);
     }
+
     @PostMapping("/updateBrightness")
-    public ResponseEntity<List<Light>> updateBrightness(@RequestBody BrightnessUpdateRequest lightsUpdatePara ) {
-        //改为使用json传值
+    public ResponseEntity<List<Light>> updateBrightness(@RequestBody BrightnessUpdateRequest lightsUpdatePara) {
+        // 改为使用json传值
         int brightness = lightsUpdatePara.getBrightness();
         List<Integer> dmxAddrList = lightsUpdatePara.getDmxAddrList();
-        //遍历 DMX 地址列表，更新每个灯光的亮度
+        // 遍历 DMX 地址列表，更新每个灯光的亮度
         try {
             for (Integer dmxAddr : dmxAddrList) {
                 System.out.println(dmxAddr);
                 lightService.UpdateBrightness(brightness, dmxAddr);
             }
-        }catch (Exception e) {
-                // 处理异常情况（可选）
-                e.printStackTrace();
+        } catch (Exception e) {
+            // 处理异常情况（可选）
+            e.printStackTrace();
         }
         List<Light> lights = lightService.findAllLights();
         return ResponseEntity.ok(lights);
     }
-  
-    
 
-//    @PostMapping
-//    public ResponseEntity<Light> createLight(@RequestBody Light light) {
-//        Light createdLight = lightService.save(light);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(createdLight);
-//    }
+    // @PostMapping
+    // public ResponseEntity<Light> createLight(@RequestBody Light light) {
+    // Light createdLight = lightService.save(light);
+    // return ResponseEntity.status(HttpStatus.CREATED).body(createdLight);
+    // }
 
     // 其他API端点
 }
